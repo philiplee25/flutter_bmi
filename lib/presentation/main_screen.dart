8e89a2a24512d05eae7e176867c0e6b8c83a3d3a
leepl <yplunap25@gmail.com>
2024-04-07 19:59:33 +0900

    feat: formKey add

---
+++ lib/presentation/main_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI 계산기'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: '키'),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '키를 입력하세요.';
+                }
+              },
+            ),
+            const SizedBox(height: 8.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '몸무게',
+              ),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '몸무게를 입력하세요.';
+                }
+              },
+            ),
+            const SizedBox(height: 8.0),
+            ElevatedButton(
+              onPressed: () {
+                if(_formKey.currentState?.validate() ?? false) {
+
+                }
+              },
+              child: const Text('결과'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
